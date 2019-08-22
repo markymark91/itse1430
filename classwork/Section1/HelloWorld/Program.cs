8c206cf710413c559cd8cfd7eb88f3695b266487
mark.dobbins <mark.dobbins@NEITD-211220>
2019-08-21 19:02:51 -0500

    Initial version

---
+++ classwork/Section1/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * ITSE 1430
+ * Lab 1
+ * Mark Dobbins
+ */
+
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
