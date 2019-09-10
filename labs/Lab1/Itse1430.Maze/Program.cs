2db2f4aac5b0b71a0ea05c6f1f276ab24dbb7412
markymark91 <54293750+markymark91@users.noreply.github.com>
2019-09-10 12:50:12 -0500

    Started Lab1, added 15 room functions

---
+++ labs/Lab1/Itse1430.Maze/Program.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * ITSE 1430
+ * Lab 1
+ * Mark Dobbins
+ */
+
+
+
+using System;
+
+namespace Itse1430.Maze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room1 ();
+        }
+
+        static void Room1 ()
+        {
+            Console.WriteLine ("You've entered Rapture, an underwater city situated at the bottom of the ocean. \nOnce a utopia for its citizens, Rapture has fallen into chaos, in part due to the mental instability that resulted from excessive gene-splicing. ");
+            Console.WriteLine ("Your goal is to escape Rapture. There is a room with a bathysphere, which will carry you back to the surface.");
+            Console.WriteLine ("You find yourself in the welcome lobby. Loose rubble is scattered across the flooded floor, water leaking from cracked windows and busted pipes. \nThere are three pathways connected to this room; Left, Right, and Forward");
+        }
+        static void Room2 ()
+        {
+
+        }
+        static void Room3 ()
+        {
+
+        }
+        static void Room4 ()
+        {
+
+        }
+        static void Room5 ()
+        {
+
+        }
+        static void Room6 ()
+        {
+
+        }
+        static void Room7 ()
+        {
+
+        }
+        static void Room8 ()
+        {
+
+        }
+        static void Room9 ()
+        {
+
+        }
+        static void Room10 ()
+        {
+
+        }
+        static void Room11 ()
+        {
+
+        }
+        static void Room12 ()
+        {
+
+        }
+        static void Room13 ()
+        {
+
+        }
+        static void Room14 ()
+        {
+
+        }
+        static void Room15 ()
+        {
+
+        }
+    }
+}
