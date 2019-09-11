5e78396717c4d4fadbb358074153d4d419e1c04f
markymark91 <54293750+markymark91@users.noreply.github.com>
2019-09-10 21:36:45 -0500

    Added several functionsm including 'look' support

--- labs/Lab1/Itse1430.Maze/Program.cs
+++ labs/Lab1/Itse1430.Maze/Program.cs
@@ -10,48 +10,55 @@ using System;
 
 namespace Itse1430.Maze
 {
+    enum directions { left = 2, up = 4, right = 8 };
     class Program
     {
         static void Main(string[] args)
         {
+
             Room1 ();
         }
 
-        enum Command
+        static string GetCommand()
         {
-            Quit = 1,
-            MoveForward,
-            MoveBackward
-        }
+            Console.WriteLine ("\nPlease enter a command. You may either enter 'move' followed by a direction, \n'turn' followed by a direction, or 'look' to get a description of where you are: ");
+            command = Console.ReadLine ();
+            command = command.ToLower();
 
-        static Command GetCommand()
-        {
-
-
-            return Command.Quit;
-        }
-
-        static Command ParseCommand(string message)
-        {
-            while(true)
+            while (command != "turn left" && command!= "turn right" && command != "move up" && command != "move down" && command != "move left" && command != "move right" && command != "look")
             {
-                string input = Console.ReadLine ();
-                input = input.ToLower ();
-
+                Console.WriteLine ("Invalid input. You may either enter 'move' followed by a direction, \n'turn' followed by a direction, or 'look' to get a description of where you are: ");
+                command = Console.ReadLine ();
+                command = command.ToLower ();
             }
-
-
-
+            return command;
         }
+
+        static void HandleCommand(string choice, string description)
+        {
+            switch (choice)
+            {
+                case "look": Console.WriteLine (description);
+                break;
+            }
+        }
+
         static void Room1 ()
         {
-            Console.WriteLine ("You've entered Rapture, an underwater city situated at the bottom of the ocean. \nOnce a utopia for its citizens, Rapture has fallen into chaos, in part due to the mental instability that resulted from excessive gene-splicing. ");
-            Console.WriteLine ("Your goal is to escape Rapture. There is a room with a bathysphere, which will carry you back to the surface.");
-            Console.WriteLine ("You find yourself in the welcome lobby. Loose rubble is scattered across the flooded floor, water leaking from cracked windows and busted pipes. \nThere are three pathways connected to this room; Left, Right, and Forward");
+            string description = "You've entered Rapture, an underwater city situated at the bottom of the ocean. \nOnce a utopia for its citizens," +
+                " Rapture has fallen into chaos, in part due to the \nmental instability that resulted from excessive gene-splicing. "+
+                "Your goal is to escape Rapture. \nThere is a room with a bathysphere, which will carry you back to the surface."+
+                "\nYou find yourself in the welcome lobby. Loose rubble is scattered across the flooded floor, \nwater leaking from cracked windows and busted pipes. " +
+                "\n\nThere are three pathways connected to this room; Left, Right, and Forward";
+
+            Console.WriteLine (description);
+            Console.WriteLine (directions.left);
+            string choice = GetCommand ();
+            HandleCommand (choice, description);
         }
         static void Room2 ()
         {
-
+            string description = "This is the description of this dank place";
         }
         static void Room3 ()
         {
@@ -105,5 +112,41 @@ namespace Itse1430.Maze
         {
 
         }
+        static string command;
     }
 }
+
+/*
+ * enum Command
+        {
+            Quit = 1,
+            MoveForward,
+            MoveBackward
+        }
+
+        static Command GetCommand()
+        {
+            //Get input from user
+            Command command = ParseCommand (input);
+            
+            return Command.Quit;
+        }
+
+        static Command ParseCommand(string input)
+        {
+            while (true)
+            {
+                input = Console.ReadLine ();
+                input = input.ToLower ();
+
+            }
+
+        }
+
+        static Command HandleCommand()
+        {
+            Command command = GetCommand ();
+
+            return command;
+        }
+*/
