06840aba0153e7d8f600910fa6823a486f16c8f1
mark.dobbins <mark.dobbins@NEITD-211211>
2019-09-11 19:07:38 -0500

    Began working on a new project called MovieLib, and learned about classes

--- classwork/MovieLibrary/Itse1430.MovieLib.Host/MainForm.cs
+++ classwork/MovieLibrary/Itse1430.MovieLib.Host/MainForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Itse1430.MovieLib.Host
@@ -15,6 +8,8 @@ namespace Itse1430.MovieLib.Host
         public MainForm ()
         {
             InitializeComponent ();
+
+            var movie = new Movie ();
         }
     }
 }
---
+++ classwork/MovieLibrary/Itse1430.MovieLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itse1430.MovieLibrary
+{
+    public class Class1
+    {
+    }
+}
