f73f08298e37f1029f84f94eae1371c6f84bb929
mark.dobbins <mark.dobbins@NEITD-211211>
2019-09-18 18:52:30 -0500

    Added functionality to Save and Cancel, edited the form to behand like a child window

--- classwork/MovieLibrary/Itse1430.MovieLib/Movie.cs
+++ classwork/MovieLibrary/Itse1430.MovieLib/Movie.cs
@@ -18,6 +18,27 @@ namespace Itse1430.MovieLib
         public bool hasSeen;
         public int runLength;
 
+        /// <summary>Validates the movie.</summary>
+        /// <returns>An error message if validation fails or empty string otherwise</returns>
+        public string Validate()
+        {
+            //this is implicit first parameter, represents instance
+            var title = "";
+            //Name is required
+            if (String.IsNullOrEmpty (this.title))
+                return "Name is required";
+            //Release year >= 1900
+            if (releaseYear < 1900)
+                return "Release Year must be >= 1900";
+            //Run length >= 0
+            if (runLength <= 0)
+                return "Run Length must be >=0";
+            //Rating is required
+            if (String.IsNullOrEmpty (rating))
+                return "Rating is required";
+
+            return "";
+        }
         //can new up other objects, however this wont have access to any of the cool stuff above
         //private Movie originalMovie = new Movie ();
     }
