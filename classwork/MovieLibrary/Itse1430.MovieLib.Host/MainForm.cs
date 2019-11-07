9d9f5ae18b38dfc9cbc3db75c4b51205556e3b58
mark.dobbins <mark.dobbins@NEITD-211211>
2019-11-06 19:09:24 -0600

    Finally Added Database Support!

--- classwork/MovieLibrary/Itse1430.MovieLib.Host/MainForm.cs
+++ classwork/MovieLibrary/Itse1430.MovieLib.Host/MainForm.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Configuration;
 using System.Linq;
 using System.Windows.Forms;
 using Itse1430.MovieLib.IO;
+using Itse1430.MovieLib.SqlServer;
 
 namespace Itse1430.MovieLib.Host
 {
@@ -17,11 +19,13 @@ namespace Itse1430.MovieLib.Host
         {
             base.OnLoad (e);
 
-            _movies = new FileMovieDatabase (@"movies.csv");
-            var count = _movies.GetAll ().Count ();
-            if (count == 0)
+            var connString = ConfigurationManager.ConnectionStrings["MovieDatabase"];
+            //_movies = new FileMovieDatabase (@"movies.csv");
+            _movies = new SqlMovieDatabase (connString.ConnectionString);
+            //var count = _movies.GetAll ().Count ();
+            //if (count == 0)
                 //MovieDatabaseExtensions.Seed (_movies);
-                _movies.Seed ();
+                //_movies.Seed ();
 
             UpdateUI ();
         }
